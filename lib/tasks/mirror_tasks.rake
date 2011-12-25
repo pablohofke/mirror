923b0e09fd665b7943c511fe1cc8162a091cfb9c
Pablo Hofke <pablohofke@gmail.com>
2011-12-25 13:15:23 -0200

    First commit

---
+++ lib/tasks/mirror_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :mirror do
+#   # Task goes here
+# end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'ca7b11e5998c8718e300097a0442cba7620c3e02ea333a72b5edfbf76e30010b25ba8b20240521ea8488b75cc1c5047ea755924c20750312b79ebfaf5900a42d'
