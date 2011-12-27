03c0ad32539a93c6e307ad117faafe1bdc2d4e06
Pablo Hofke <pablohofke@gmail.com>
2011-12-27 12:20:31 -0200

    Incluido Mirror no TestCase.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,17 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
+# require 'rubygems'
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
+# require 'test/unit'
+
+# $:.unshift File.expand_path("../../lib", __FILE__)
+# require 'mirror'
+
+class ActiveSupport::TestCase
+  include Mirror
+end
 
 Rails.backtrace_cleaner.remove_silencers!
 
