8c68e52f72006341516d85da28c45a78de99d7c9
Pablo Hofke <pablohofke@gmail.com>
2012-01-10 06:40:24 -0200

    Validator completo.

--- lib/mirror/validator.rb
+++ lib/mirror/validator.rb
@@ -1,41 +1,22 @@
 # Representa o validator
 module Mirror
   class Validator
-    attr_accessor :model
+    
+    include Mirror::Helpers
+    
+    attr_writer :model
+    # Retorna as mensagens do validator
+    attr_reader :message
     
     def initialize(model=nil)
       @model= model
     end
     
-    # Verifica a existencia do validator.kind para o attribute
-    # has_kind? :email, :presence
-    # has_kind? :email, [:presence,:uniqueness]
-    def has_kind?(attribute,kinds)
-      kinds_to_check,result=[],[]
-      kinds_to_check << kinds
-      kinds_to_check.flatten!
-      kinds_to_check.each{|k| result << check_kind(attribute,k)}
-      !result.include?(false)
+    # Retorna array com os tipos de validator
+    # kinds :email
+    def kinds(attribute)
+      @model.validators_on(attribute).map(&:kind)
     end
-    
-    # Verifica existencias das options para o attribute
-    # has_options?(:attribute)
-    # has_options?(:attribute, :kind => :value)
-    # has_options?(:attribute, :kind => :value, :options =>{...})
-    # has_options?(:attribute, :options => {...})
-    def has_options?(attribute,args={})
-      kind=args[:kind] if args[:kind]
-      options_to_check=normalize_options args[:options] if args[:options]
-      if args=={}
-        !options(attribute).empty?
-      elsif kind && !options_to_check
-        !options(attribute,kind).empty?
-      elsif kind && options_to_check
-        (options_to_check.to_a - options(attribute,kind).to_a).empty?
-      elsif !kind && options_to_check
-        (options_to_check.to_a - options(attribute).to_a).empty?
-      end  
-     end
     
     # Retorna as options do validator ou do attribute
     # options :email, :presence
@@ -51,6 +32,42 @@ module Mirror
       end
     end
     
+    # Verifica a existencia do validator.kind para o attribute
+    # has_kind? :email, :presence
+    # has_kind? :email, [:presence,:uniqueness]
+    def has_kind?(attribute,kinds)
+      unless (no_included_kinds=Array(kinds) - kinds(attribute)).empty?
+        @message="#{attribute.to_s} does not include #{sentence(no_included_kinds)} #{inflect("validator",no_included_kinds.size)}"
+        false
+      else
+        true
+      end
+    end
+    
+    # Verifica existencias das options para o attribute
+    # has_options?(:attribute)
+    # has_options?(:attribute, :kind => :value)
+    # has_options?(:attribute, :kind => :value, :options =>{...})
+    # has_options?(:attribute, :options => {...})
+    def has_options?(attribute,args={})
+      kind=args[:kind] if args[:kind]
+      options_to_check=normalize_options args[:options] if args[:options]
+      current_options=options(attribute,kind) || {}
+      diff_options=Array(options_to_check) - Array(current_options)
+      if args=={} && current_options.empty?
+        false & @message="#{attribute.to_s} does not have options"
+      elsif kind && !options_to_check && current_options.empty?
+        false & @message="#{attribute.to_s} does not have options for #{kind.to_s}"
+      elsif kind && options_to_check && !diff_options.empty?
+        # debugger if current_options.nil?
+        false & @message="#{attribute.to_s} does not have #{sentence((options_to_check.keys - current_options.keys))} #{inflect("option",diff_options.size)} for #{kind.to_s}"
+      elsif !kind && options_to_check && !diff_options.empty?
+        false & @message="#{attribute.to_s} does not have #{sentence((options_to_check.keys - current_options.keys))} #{inflect("option",diff_options.size)}"
+      else
+        true
+      end
+     end
+    
     protected
     
     # Reorganiza a hash options quando ela apresenta diferença na escrita e o que é passado
@@ -62,17 +79,6 @@ module Mirror
         options[:maximum] -= 1 if range.exclude_end?
       end
       options
-    end
-    
-    # Verifica se existe o validator kind para o attribute 
-    def check_kind(attribute,kind)
-      kind_validators=[]
-      if (class_validators=@model.validators_on(attribute).map(&:class))
-        class_validators.each{|v| kind_validators << v.kind}
-        kind_validators.include?(kind)
-      else
-        false
-      end
     end
     
   end
--- test/validator_test.rb
+++ test/validator_test.rb
@@ -6,26 +6,42 @@ class ValidatorTest < ActiveSupport::TestCase
     @uv=Validator.new(User)
   end
   
-  # has_kind
-  test "deve validar os validadores padrão" do
+  # kinds
+  test "kinds deve retornar array com kind validators" do
+    assert_equal [:presence,:uniqueness,:email],@uv.kinds(:email)
+    assert_equal [:presence,:confirmation],@uv.kinds(:email_confirmation)
+    assert_equal [:presence,:length],@uv.kinds(:password)
+  end
+  
+  # has_kind?
+  test "has_kind? deve validar os validadores padrão" do
     assert @uv.has_kind?(:email,:presence)
     assert @uv.has_kind?(:email,:uniqueness)
     assert @uv.has_kind?(:password,:presence)
     assert !@uv.has_kind?(:password,:uniqueness)
   end
   
-  test "deve validar vários validadores para o mesmo atributo" do
+  test "has_kind? deve validar vários validadores para o mesmo atributo" do
     assert @uv.has_kind?(:password,[:presence,:length])
     assert !@uv.has_kind?(:password,[:presence,:length,:uniqueness])
   end
   
-  test "deve validar mesmo que nem todos os validadores sejam contemplados" do
+  test "has_kind? deve validar mesmo que nem todos os validadores sejam contemplados" do
     assert @uv.has_kind?(:email,[:presence,:uniqueness])
   end
   
-  test "deve validar custom validators" do
+  test "has_kind? deve validar custom validators" do
     assert @uv.has_kind?(:email,:email)
     assert @uv.has_kind?(:email,[:email,:presence,:uniqueness])
+  end
+  
+  test "has_kind? deve retornar a mensagem adequada" do
+    @uv.has_kind?(:email,:presence)
+    assert_nil @uv.message
+    @uv.has_kind?(:password,:uniqueness)
+    assert_equal "password does not include uniqueness validator",@uv.message
+    @uv.has_kind?(:password,[:presence,:length,:uniqueness,:confirmation,:exclusion])
+    assert_equal "password does not include uniqueness, confirmation and exclusion validators",@uv.message
   end
   
   # options
@@ -45,25 +61,46 @@ class ValidatorTest < ActiveSupport::TestCase
   
   # has_options?
   
-  test "has_options? :attribute" do
+  # has_options?(:attribute)
+  test "has_options? deve aceitar somente o attribute e verificar se existe qualquer option" do
     assert @uv.has_options?(:password)
     assert !@uv.has_options?(:name)
   end
   
-  test "has_options?(:attribute, :kind => :value)" do
+  # has_options?(:attribute, :kind => :validator_kind)
+  test "has_options? deve verificar se existem options para um kind específico do attribute" do
     assert @uv.has_options?(:password, :kind => :length)
     assert !@uv.has_options?(:email, :kind => :presence)
   end
   
-  test "has_options?(:attribute, :kind => :value, :options =>{...})" do
+  # has_options?(:attribute, :kind => :value, :options =>{...})
+  test "has_options? deve verificar as options específicas para o attribute e kind" do
     assert @uv.has_options?(:password, {:kind => :length, :options => {:within => 6..30}})
     assert !@uv.has_options?(:password, {:kind =>:length, :options => {:within => 6..30, :on => :create}})
     assert !@uv.has_options?(:email, {:kind =>:length, :options => {:within => 6..30}})
   end
   
-  test "has_options?(:attribute, :options => {...})" do
+  # has_options?(:attribute, :options => {...})
+  test "has_options? deve verificar se existem options relacionadas ao attribute" do
     assert @uv.has_options?(:email_confirmation, :options =>{:on => :create})
     assert !@uv.has_options?(:email, :options => {:on => :create})
+  end
+  
+  test "has_options? deve gerar as mensagens corretas" do
+    assert @uv.has_options?(:password)
+    assert_nil @uv.message
+    @uv.has_options?(:name)
+    assert_equal "name does not have options", @uv.message
+    @uv.has_options?(:email, :kind => :presence)
+    assert_equal "email does not have options for presence", @uv.message
+    @uv.has_options?(:password, {:kind =>:length, :options => {:within => 6..30, :on => :create, :message => "Any message", :allow_blank => true}})
+    assert_equal "password does not have on, message and allow_blank options for length", @uv.message
+    @uv.has_options?(:password, {:kind =>:length, :options => {:within => 6..30, :on => :create}})
+    assert_equal "password does not have on option for length", @uv.message
+    @uv.has_options?(:email, :options => {:on => :create})
+    assert_equal "email does not have on option", @uv.message
+    @uv.has_options?(:email, :options => {:on => :create, :message => "Any message", :allow_blank => true})
+    assert_equal "email does not have on, message and allow_blank options", @uv.message
   end
   
 end
