277bbbb8dedd3ed9cadb8fc21f8c585d1c865d21
Pablo Hofke <pablohofke@gmail.com>
2012-01-10 06:44:28 -0200

    iniciado o trabalho com assertions, ainda não funcional

--- lib/mirror/assertions.rb
+++ lib/mirror/assertions.rb
@@ -2,10 +2,16 @@ module Mirror
   module Assertions
     extend ActiveSupport::Concern
       included do
-        def assert_validation_on(attribute)
-          true
+        def assert_validation_on(attribute,*args)
+          # av=Mirror::Validator.new self.class.name.gsub(/Test/, "").constantize
+          debugger
+          assert true
         end
       end
+      
+      # def get_options(type)
+      #         
+      #       end
   end  
 end
 ActiveSupport::TestCase.send(:include,Mirror::Assertions)
--- test/dummy/test/unit/user_test.rb
+++ test/dummy/test/unit/user_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "validations" do
+    # debugger
     assert_validation_on(:email, :presence, :uniqueness, :email)
     assert_validation_on(:email_confirmation, :presence, :confirmation, :on => :create)
     assert_validation_on(:password, :presence, :length => {:within => 6..30})
