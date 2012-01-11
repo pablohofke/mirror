8b4437389c55f3c8907c19387c79c102b805f148
Pablo Hofke <pablohofke@gmail.com>
2012-01-11 12:10:45 -0200

    assert_validation_on verifica a existência dos validators kinds

--- lib/mirror/assertions.rb
+++ lib/mirror/assertions.rb
@@ -3,15 +3,32 @@ module Mirror
     extend ActiveSupport::Concern
       included do
         def assert_validation_on(attribute,*args)
-          # av=Mirror::Validator.new self.class.name.gsub(/Test/, "").constantize
-          debugger
-          assert true
+          result, message=[],[]
+          av=Mirror::Validator.new self.class.name.gsub(/Test/, "").constantize
+          kinds=get_options(:kinds,*args)
+          options=get_options(:options,*args)
+          # debugger
+          result << av.has_kind?(attribute,kinds)
+          result << av.has_options?(attribute,options)
+          message << av.message
+          assert_block message.first do
+            !result.include?(false)
+          end
         end
       end
       
-      # def get_options(type)
-      #         
-      #       end
+      def get_options(type,*args)
+        validations_options=[:allow_nil,:allow_blank,:message,:on]
+        case type
+        when :kinds
+          result=[]
+          args.each{|a| result << a unless validations_options.include?(a)}
+        when :options
+          result={}
+          args.each{|a| if a.is_a?(Hash) then result=result.merge(a) end}
+        end
+        result            
+      end
   end  
 end
 ActiveSupport::TestCase.send(:include,Mirror::Assertions)
--- test/dummy/test/unit/user_test.rb
+++ test/dummy/test/unit/user_test.rb
@@ -3,9 +3,24 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "validations" do
-    # debugger
-    assert_validation_on(:email, :presence, :uniqueness, :email)
-    assert_validation_on(:email_confirmation, :presence, :confirmation, :on => :create)
+    
     assert_validation_on(:password, :presence, :length => {:within => 6..30})
   end
+  
+  # assert_validation_on
+  test "assert_validation_on deve verificar a existencia dos kinds" do
+    assert_validation_on(:email, :presence, :uniqueness, :email)
+    begin
+      assert_validation_on(:email, :presence, :uniqueness, :email, :length)
+      fail 'should not get to here'
+    rescue Exception => e
+      assert_equal "email does not include length validator", e.message
+    end
+  end
+  
+  test "assert_validation_on deve confirma a existência das options dos attributes" do
+    assert_validation_on(:email_confirmation, :presence, :confirmation, :on => :create)
+  end
+  
+  
 end
