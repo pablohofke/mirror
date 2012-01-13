be8ec722b8dfcbcab171012444ad8d3c30772494
Pablo Hofke <pablohofke@gmail.com>
2012-01-13 11:47:59 -0200

    incluido assert_validation

--- test/dummy/test/test_helper.rb
+++ test/dummy/test/test_helper.rb
@@ -6,4 +6,16 @@ require 'mirror'
 
 class ActiveSupport::TestCase
   
+  # Testa a falha de um assert_validation_on
+  # assert_fail_validaton "assertion error message" do
+  #   assert_validation_on(...)
+  # end
+  def assert_fail_validation(expected_message,&block)
+    begin
+      block.call
+      fail 'should not get to here'
+    rescue Exception => e
+      assert_equal expected_message, e.message
+    end
+  end
 end
