684667e45de1987543085900e70c72863efd6d91
Pablo Hofke <pablohofke@gmail.com>
2012-01-16 09:23:24 -0200

    assertions documentadas

--- lib/mirror/assertions.rb
+++ lib/mirror/assertions.rb
@@ -2,6 +2,13 @@ module Mirror
   module Assertions
     extend ActiveSupport::Concern
       included do
+        
+        # Verifica se foi definida a validação para o attribute específico
+        # assert_validation_on(:attribute, :kind, :kind...)
+        # assert_validation_on(:attribute, :kind, kind...,:option => :value, :option => :value...)
+        # assert_validation_on(:attribute, :kind, :kind => :option...)
+        # Para passar mensagem, sempre coloque no final
+        # assert_validation_on(:attribute, :kind, :kind..., message)
         def assert_validation_on(attribute,*args)
           result, message=[],[]
           av=Mirror::Validator.new get_model
@@ -11,8 +18,6 @@ module Mirror
           result << av.has_kind?(attribute,kinds)
           result << av.has_options?(attribute,:options => attribute_options)
           kind_options.each do |k|
-            # debugger if k[:kind]==:presence
-            Rails.logger.debug "kind: #{k[:kind]} | options: #{k[:options]}"
             result << av.has_options?(attribute,:kind => k[:kind], :options => k[:options])
             message << av.message
           end
@@ -23,21 +28,26 @@ module Mirror
         end
       end
       
+      # Verifica se xiste o tipo de relação no model
+      # assert_has_many :models, :models...[, message]
       def assert_has_many(*args)
-        aa=Mirror::Association.new get_model
-        names=get_association_options(:names,*args)
-        result= aa.has_many? *names
-        assert_block (get_association_options(:message,*args) || aa.message) do
-          result
-        end
+        assert_association(:has_many,*args)
+      end
+
+      # Verifica se xiste o tipo de relação no model
+      # assert_belongs_to :models, :models...[, message]
+      def assert_belongs_to(*args)
+        assert_association(:belongs_to,*args)
       end
       
       protected
       
+      # Obtem o model do teste
       def get_model
         self.class.name.gsub(/Test/, "").constantize
       end
       
+      # Obtem as options para assert_validation_on
       def get_validation_on_options(type,*args)
         validations_options=[:allow_nil,:allow_blank,:message,:on]
         case type
@@ -70,6 +80,20 @@ module Mirror
         result            
       end
       
+      # Método genérico para ser usado com assert_has_many e assert_block
+      # assert_association :has_many,*args
+      # assert_association :belongs_to,*args
+      def assert_association(type,*args)
+        aa=Mirror::Association.new get_model
+        names=get_association_options(:names,*args)
+        result= aa.send((type.to_s + "?").to_sym, *names)
+        assert_block (get_association_options(:message,*args) || aa.message) do
+          result
+        end
+        
+      end
+      
+      # Obtem as óptions para assert_association
       def get_association_options(type,*args)
         case type
         when :names
